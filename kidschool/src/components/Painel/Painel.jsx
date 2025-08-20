87826867541c88815708ef7518da4242bc9a5c52
NICOLAS BONFIM MORATO <045689@americana.fatec.br>
2025-08-20 17:18:50 -0300

    dasjdsahjdj

--- kidschool/src/components/Painel/Painel.jsx
+++ kidschool/src/components/Painel/Painel.jsx
@@ -4,6 +4,7 @@ import styles from './Painel.module.css';
 const Painel = () => {
 return (
         <div className={styles.faixaRoxa}>
+            <h1 className={styles.texto}>Aprenda enquanto evolui seu personagem!</h1>
         </div>
     );
 };
