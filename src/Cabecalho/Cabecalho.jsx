4836b768e685b0a86bc79294968922796f2b9d2a
Lawliet1200 <lopes.lucas2509@gmail.com>
2025-09-04 21:42:04 -0300

    sladkaldsald

---
+++ src/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,22 @@
+import styles from './Cabecalho.module.css';
+import KidschoolLogo from '../../assets/kidschool_logo.png';
+
+const Cabecalho = () => {
+    return (
+        <header className={styles.cabecalho}>
+            <nav className={styles.nav}>
+                    <div className={styles.navLeft}>
+                    <img src={KidschoolLogo} alt="Kidschool" className={styles.logo}/>
+                    </div>
+                    <div className={styles.navRight}>
+                    <a href="#login">Login</a>
+                    <a href="#sobre">Sobre Nós</a>
+                    <a href="#aprender">Aprender</a>
+                    <a href="#contato">Contato</a>
+                    </div>
+                </nav>
+        </header>
+    )
+}
+
+export { Cabecalho };
